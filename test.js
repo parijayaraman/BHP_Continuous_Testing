ab40e1cea7770c3b9f78c6bc71c7f055129658ec
Pari J <pari@riverstonetech.com>
2018-09-06 17:49:51 +0530

    testjs updated

--- test.js
+++ test.js
@@ -3,10 +3,25 @@
 /**
  * Module dependencies.
  */
-
+var webdriver = require('selenium-webdriver');
 var path = require('path');
 var app = require(path.resolve('./config/lib/app'));
 
-app.init(function () {
+/**app.init(function () {
   console.log('Initialized test automation');
+*/
+
+var browser = new webdriver.Builder().
+	withCapabilities(webdriver.Capabilities.chrome()).build());
+	
+browser.get('htt://localhost:3000');
+
+var title = browser.getTitle();
+
+console.log(title);
+
+assert.equal(title, 'Birthday Headlines');
+
+console.log('Test completed');
+  
 });
