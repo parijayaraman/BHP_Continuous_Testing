cc75807879b9618d899b351bd59ea6fc413702a5
Pari J <pari@riverstonetech.com>
2018-09-07 12:10:17 +0530

    Update spec file

--- tests/spec.js
+++ tests/spec.js
@@ -1,12 +1,33 @@
 describe('Protractor Demo App', function() {
-  it('should add one and two', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+
+  beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+  });
 
-    element(by.id('gobutton')).click();
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
 
-    expect(element(by.binding('latest')).getText()).
-        toEqual('5'); // This is wrong!
+  it('should add one and two', function() {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', function() {
+    // Fill this in.
+    expect(latestResult.getText()).toEqual('10');
+  });
+
+  it('should read the value from an input', function() {
+    firstNumber.sendKeys(1);
+    expect(firstNumber.getAttribute('value')).toEqual('1');
   });
 });
